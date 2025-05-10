43458ab12208c56fd3f95f752d4a76edd0681e8c
Styven Manaja <139322152+StyvenManaja@users.noreply.github.com>
2025-05-10 11:30:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const app = require('./app');
 const db = require('./src/config/db');
 
-const PORT = process.env.PORT || 4000;
-
 db.connectToDB();
 
-app.listen(PORT, () => {
-    console.log(`Server launched at port : ${PORT}`);
-})
+module.exports = app;
